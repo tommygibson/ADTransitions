8107680286bb631250b1216484fb935765eab020
Tommy Gibson <tommy.a.gibson@gmail.com>
2021-06-04 14:55:28 -0700

    Update grid search
    Fewer candidate initial values, less strict convergence criteria

--- gird.search.R
+++ gird.search.R
@@ -26,23 +26,26 @@ lb <- rep(c(-14, 0.001), 12)
 
 ub <- rep(c(-4, 0.15), 12)
 
+## Grid of initial values
 Lk0.init <- seq(-12, -6, 2)
 a <- length(Lk0.init)
 k1.init <- seq(0.02, 0.1, length.out = a)
 
 possible.inits <- as.matrix(expand.grid(Lk0.init, k1.init))
 
+
 init.valid <- vector(length = (a * a))
-init.loss <- vector(0, length = (a * a))
+init.loss <- vector(length = (a * a))
 
 for(i in 1:(a * a)){
   g <- eval_g_ineq_both(rep(possible.inits[i,], 12), 0, 0, 0) < 0
   if(any(g == FALSE)){
     init.valid[i] <- 0
+    init.loss[i] <- NA
   } else {
     init.valid[i] <- 1
     init.loss[i] <- eval_f_logs_weighted(x = rep(possible.inits[i,], 12), 
-                                      r45.params, avg_prev_u, incidence = empirical.incidence, w = 1)
+                                         r45.params, avg_prev_u, incidence = empirical.incidence, w = 1)
   }
 }
 
@@ -50,7 +53,7 @@ for(i in 1:(a * a)){
 ###### of the constraints, so this little precursor optimization accounts for that
 for(i in 1:(a * a)){
   
-  if(init.valid[i] == 0 ) next
+  if(init.valid[i] == 0) next
   else{
     opt.temp <- nloptr(x0 = rep(possible.inits[i,], 12), 
                         eval_f = eval_f_logs_weighted, 
@@ -58,42 +61,131 @@ for(i in 1:(a * a)){
                         eval_g_ineq = eval_g_ineq_weighted,
                         opts = list("algorithm"="NLOPT_LN_COBYLA",
                                     "xtol_rel"=1e-3,
-                                    "maxeval"=5),
+                                    "maxeval"=10),
                         r45 = r45.params,
                         prevs = avg_prev_u,
                         incidence = empirical.incidence,
                         w = 1)
-    if(is.nan(opt.temp$objective)) init.valid[i] <- 0
-    
+    if(is.nan(opt.temp$objective)){ 
+      init.valid[i] <- 0
+      init.loss[i] <- NA
+    }
   }
   
 }
 
-opts <- list()
+opts.grid <- list()
+
 index <- 1
 for(i in 1:(a * a)){
-  
   if(init.valid[i] == 0) next
   else{
-    opts[[index]] <- nloptr(x0 = rep(possible.inits[i,], 12), 
-                       eval_f = eval_f_logs_weighted, 
-                       lb = lb, ub = ub, 
-                       eval_g_ineq = eval_g_ineq_weighted,
-                       opts = list("algorithm"="NLOPT_LN_COBYLA",
-                                   "xtol_rel"=1e-3,
-                                   "maxeval"=1000),
-                       r45 = r45.params,
-                       prevs = avg_prev_u,
-                       incidence = empirical.incidence,
-                       w = 1)
+    opts.grid[[index]] <- nloptr(x0 = rep(possible.inits[i,], 12), 
+                               eval_f = eval_f_logs_weighted, 
+                               lb = lb, ub = ub, 
+                               eval_g_ineq = eval_g_ineq_weighted,
+                               opts = list("algorithm"="NLOPT_LN_COBYLA",
+                                           "xtol_rel"=5e-3,
+                                           "maxeval"=15000),
+                               r45 = r45.params,
+                               prevs = avg_prev_u,
+                               incidence = empirical.incidence,
+                               w = 1)
     index <- index + 1
   }
-  
 }
 
-init.value.valid <- cbind(possible.inits, init.valid)
+#### Okay, we'll start with optimizing for 500 iterations, weed some out, 
+# then 1000 iterations, weed out again, yada yada
+##### List of all optimizations
+# opts.500 <- list()
+# opts.1000 <- list()
+# index <- 1
+# for(i in 1:(a * a)){
+#   
+#   if(init.valid[i] == 0) next
+#   else{
+#     opts.500[[index]] <- nloptr(x0 = rep(possible.inits[i,], 12), 
+#                        eval_f = eval_f_logs_weighted, 
+#                        lb = lb, ub = ub, 
+#                        eval_g_ineq = eval_g_ineq_weighted,
+#                        opts = list("algorithm"="NLOPT_LN_COBYLA",
+#                                    "xtol_rel"=5e-3,
+#                                    "maxeval"=500),
+#                        r45 = r45.params,
+#                        prevs = avg_prev_u,
+#                        incidence = empirical.incidence,
+#                        w = 1)
+#     opts.1000[[index]] <- nloptr(x0 = rep(possible.inits[i,], 12), 
+#                                 eval_f = eval_f_logs_weighted, 
+#                                 lb = lb, ub = ub, 
+#                                 eval_g_ineq = eval_g_ineq_weighted,
+#                                 opts = list("algorithm"="NLOPT_LN_COBYLA",
+#                                             "xtol_rel"=5e-3,
+#                                             "maxeval"=1000),
+#                                 r45 = r45.params,
+#                                 prevs = avg_prev_u,
+#                                 incidence = empirical.incidence,
+#                                 w = 1)
+#     index <- index + 1
+#   }
+#   
+# }
+# 
+# ##### Do it again with 3000 iterations, only those qualifying are in
+# opts.2000 <- list()
+# 
+# index <- 1
+# for(i in 1:(a * a)){
+#   if(init.valid[i] == 0) next
+#   else{
+#     opts.2000[[index]] <- nloptr(x0 = rep(possible.inits[i,], 12), 
+#                                 eval_f = eval_f_logs_weighted, 
+#                                 lb = lb, ub = ub, 
+#                                 eval_g_ineq = eval_g_ineq_weighted,
+#                                 opts = list("algorithm"="NLOPT_LN_COBYLA",
+#                                             "xtol_rel"=1e-3,
+#                                             "maxeval"=2000),
+#                                 r45 = r45.params,
+#                                 prevs = avg_prev_u,
+#                                 incidence = empirical.incidence,
+#                                 w = 1)
+#     index <- index + 1
+#   }
+# }
 
-init.value.valid[,1:2][init.value.valid[,3] == 1]
+####### save results
+# saveRDS(opts.500, "OptResults/grid.500.rds")
+# saveRDS(opts.1000, "OptResults/grid.1000.rds")
+# saveRDS(opts.2000, "OptResults/grid.2000.rds")
+#######
 
+##### OBJECTIVE FUNCTION VALUES
+
+obj.500 <- obj.1000 <- obj.2000 <- vector(length = length(opts.500))
+
+
+for(i in 1:length(opts.500)){
+  obj.500[i] <- opts.500[[i]]$objective
+  obj.1000[i] <- opts.1000[[i]]$objective
+  obj.2000[i] <- opts.2000[[i]]$objective
+}
+
+## make a pretty plot
+obj.prog <- cbind.data.frame(c(init.loss[!is.na(init.loss)], obj.500, obj.1000, obj.2000),
+                             rep(c(0, 500, 1000, 2000), each = sum(!is.na(init.loss))),
+                             rep(1:sum(init.valid), 4))
+
+names(obj.prog) <- c("Objective", "Iterations", "Init Set")
+
+obj.prog %>%
+  ggplot(aes(x = Iterations, y = Objective, group = `Init Set`)) +
+  geom_point() +
+  geom_line() +
+  theme_bw() +
+  scale_y_continuous(trans = "log2") +
+  labs(title = "Convergence of COBYLA algorithm for multiple sets of initial values",
+       y = "Objective function (log scale)") +
+  scale_x_continuous(breaks = c(0, 500, 1000, 2000))
 
 
